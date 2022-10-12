923f8d9d841e898f576b765e6827d42bc3be586a
saurav <navdharesaurav2003@gmail.com>
2022-10-12 22:12:20 +0530

    resolved callback hell in all_posts function

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,18 +7,18 @@ const authenticateUser = (req, res) => {
     const { email, password } = req.body;
     User.findOne({ email: email }, (err, user) => {
         if (err) {
-            res.status(500).send("Server Error");
+            res.status(500).json({"message":"Server Error"});
         }
         else if (!user) {
-            res.status(404).send("User not found");
+            res.status(404).json({"message":"User not found"});
         }
         else {
             bcrypt.compare(password, user.password, (err, result) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!result) {
-                    res.status(401).send("Invalid Password");
+                    res.status(401).json({"message":"Invalid Password"});
                 }
                 else {
                     const accessToken = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
@@ -33,30 +33,38 @@ const follow = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             User.findOne({ username: decoded.username }, (err, user) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!user) {
-                    res.status(404).send("User not found");
+                    res.status(404).json({"message":"User not found"});
                 }
                 else {
+                    if(req.params.id === user._id){
+                        res.status(400).json({"message":"Cannot follow yourself"});
+                        return;
+                    }
                     User.findById(req.params.id, (err, userToFollow) => {
                         if (err) {
-                            res.status(500).send("Server Error");
+                            res.status(500).json({"message":"Server Error"});
                         }
                         else if (!userToFollow) {
-                            res.status(404).send("User not found");
+                            res.status(404).json({"message":"User not found"});
                         }
                         else {
+                            if(user.following.includes(userToFollow.username)){
+                                res.status(400).json({"message":"You are already following this user"});
+                                return;
+                            }
                             user.following.push(userToFollow.username);
                             userToFollow.followers.push(user.username);
                             user.save();
                             userToFollow.save();
-                            res.status(200).send("Followed");
+                            res.status(200).json({"message":"Followed"});
                         }
                     });
                 }
@@ -69,32 +77,36 @@ const unfollow = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             User.findOne({ username: decoded.username }, (err, user) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!user) {
-                    res.status(404).send("User not found");
+                    res.status(404).json({"message":"User not found"});
                 }
                 else {
                     User.findById(req.params.id, (err, userToFollow) => {
                         if (err) {
-                            res.status(500).send("Server Error");
+                            res.status(500).json({"message":"Server Error"});
                         }
                         else if (!userToFollow) {
-                            res.status(404).send("User not found");
+                            res.status(404).json({"message":"User not found"});
                         }
                         else {
                             // user.following.push(userToFollow.username);
                             // userToFollow.followers.push(user.username);
+                            if(!user.following.includes(userToFollow.username)){
+                                res.status(400).json({"message":"You are not following this user"});
+                                return;
+                            }
                             user.following = user.following.filter((username) => username !== userToFollow.username);
                             userToFollow.followers = userToFollow.followers.filter((username) => username !== user.username);
                             user.save();
                             userToFollow.save();
-                            res.status(200).send("Unfollowed");
+                            res.status(200).json({"message":"Unfollowed"});
                         }
                     });
                 }
@@ -107,15 +119,15 @@ const userProfile = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             User.findOne({ username: decoded.username }, (err, user) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!user) {
-                    res.status(404).send("User not found");
+                    res.status(404).json({"message":"User not found"});
                 }
                 else {
                     followersCount = user.followers.length;
@@ -136,23 +148,23 @@ const deletePost = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             Posts.findById(req.params.id, (err, post) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!post) {
-                    res.status(404).send("Post not found");
+                    res.status(404).json({"message":"Post not found"});
                 }
                 else {
                     if (post.username === decoded.username) {
                         post.delete();
-                        res.status(200).send("Post deleted");
+                        res.status(200).json({"message":"Post deleted"});
                     }
                     else {
-                        res.status(401).send("Unauthorized");
+                        res.status(401).json({"message":"Unauthorized"});
                     }
                 }
             });
@@ -164,18 +176,17 @@ const getPost = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             Posts.findById(req.params.id, (err, post) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!post) {
-                    res.status(404).send("Post not found");
+                    res.status(404).json({"message":"Post not found"});
                 }
                 else {
-                    console.log(post._id);
                     data = {
                         id: post._id,
                         numberOfLikes: post.likedBy.length,
@@ -192,20 +203,24 @@ const like = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             Posts.findById(req.params.id, (err, post) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!post) {
-                    res.status(404).send("Post not found");
+                    res.status(404).json({"message":"Post not found"});
                 }
                 else {
+                    if (post.likedBy.includes(decoded.username)) {
+                        res.status(400).json({"message":"Already liked"});
+                        return;
+                    }
                     post.likedBy.push(decoded.username);
                     post.save();
-                    res.status(200).send("Liked");
+                    res.status(200).json({"message":"Liked"});
                 }
             });
         }
@@ -216,24 +231,23 @@ const unlike = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             Posts.findById(req.params.id, (err, post) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!post) {
-                    res.status(404).send("Post not found");
+                    res.status(404).json({"message":"Post not found"});
                 }
                 else {
-                    console.log(post.likedBy);
                     if(!post.likedBy.includes(decoded.username)){
-                        return res.status(400).send("Bad Request");
+                        return res.status(400).json({"message":"Bad Request"});
                     }
                     post.likedBy = post.likedBy.filter((username) => username !== decoded.username);
                     post.save();
-                    res.status(200).send("Unliked");
+                    res.status(200).json({"message":"Unliked"});
                 }
             });
         }
@@ -244,15 +258,15 @@ const addComment = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
             Posts.findById(req.params.id, (err, post) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!post) {
-                    res.status(404).send("Post not found");
+                    res.status(404).json({"message":"Post not found"});
                 }
                 else {
                     const { comment } = req.body;
@@ -262,7 +276,7 @@ const addComment = (req, res) => {
                     });
                     commented.save((err, comment) => {
                         if (err) {
-                            res.status(500).send("Server Error");
+                            res.status(500).json({"message":"Server Error"});
                         }
                         else {
                             post.comments.push(comment._id);
@@ -276,70 +290,62 @@ const addComment = (req, res) => {
     })
 }
 
-const all_posts = (req, res) => {       //Remove Async Behavior: Callback hell
+const all_posts = (req, res) => { 
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
-             Posts.find({ username: decoded.username }).sort({ Date: -1 }).exec((err, posts) => {
+            Posts.find({ username: decoded.username }).sort({ uploadTime: "desc" }).exec(async(err, posts) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else {
-                    myPosts = [];
-                    posts.forEach((post) => {
-                        allComments = [];
-                        post.comments.forEach((comment) => {          //Remove Async Behavior from here
-                            Comment.findById(comment, (err, foundComment) => {
-                                if (err) {
-                                    res.status(500).send("Server Error");
-                                }
-                                else {
-                                    // console.log(foundComment);
-                                    allComments.push(foundComment.comment);
-                                    console.log(allComments);
-                                }
-                            });
-                            console.log(allComments);
-                            myPosts.push({
-                                _id: post._id,
-                                title: post.title,
-                                desc: post.description,
-                                created_at: post.uploadTime,
-                                likes: post.likedBy.length,
-                                comments: allComments,
-                            });
-                        });
-                    });
-                    if(myPosts.length == 0){
-                        res.status(200).json({
-                            message: "No posts found"
-                        });
-                        return;
+                    let data = [];
+                    if(posts.length === 0){
+                        return res.status(200).json({"message": "No Posts"});
                     }
-                    res.status(200).json(myPosts);
+                    for (let i = 0; i < posts.length; i++) {
+                        let allComments = [];
+                        for(let j=0; j<posts[i].comments.length; j++){
+                            await Comment.findById(posts[i].comments[j])
+                            .then((comment) => {
+                                allComments.push(comment.comment);
+                            })
+                            .catch((err) => {
+                                res.status(500).json({"message":"Server Error"});
+                            });
+                        }
+                        const newData = {
+                            id: posts[i]._id,
+                            title: posts[i].title,
+                            description: posts[i].description,
+                            numberOfLikes: posts[i].likedBy.length,
+                            comments: allComments
+                    }
+                    data.push(newData);
                 }
-            });
-        }
-    })
+                res.status(200).json(data);
+            }
+        });
+    }
+});
 }
 
 const addPost = (req, res) => {
     const accessToken = req.headers.authorization.split(" ")[1]; //assuming token is sent as Bearer <token>
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            res.status(401).send("Invalid Token");
+            res.status(401).json({"message":"Invalid Token"});
         }
         else {
-            console.log(decoded.username);
             User.findOne({ username: decoded.username }, (err, user) => {
                 if (err) {
-                    res.status(500).send("Server Error");
+                    res.status(500).json({"message":"Server Error"});
                 }
                 else if (!user) {
-                    res.status(404).send("User not found");
+                    res.status(404).json({"message":"User not found"});
                 }
                 else {
                     const { title, description } = req.body;
@@ -353,16 +359,16 @@ const addPost = (req, res) => {
                     });
                     post.save((err, post) => {
                         if (err) {
-                            res.status(500).send("Server Error");
+                            res.status(500).json({"message":"Server Error"});
                         }
                         else {
                             //include post ID in post
                             User.findOne({ username: decoded.username }, (err, user) => {
                                 if (err) {
-                                    res.status(500).send("Server Error");
+                                    res.status(500).json({"message":"Server Error"});
                                 }
                                 else if (!user) {
-                                    res.status(404).send("User not found");
+                                    res.status(404).json({"message":"User not found"});
                                 }
                                 else {
                                     user.posts.push(post._id);
@@ -388,12 +394,13 @@ const register = (req,res) => {
         followers: [],
         following: [],
     });
+    const accessToken = jwt.sign({username, email, password:hashedPassword}, process.env.JWT_SECRET);
     user.save((err, user) => {
         if (err) {
             res.status(500).send("Server Error");
         }
         else {
-            res.status(200).json(user);
+            res.status(200).json({user, token: accessToken});
         }
     });
 }
